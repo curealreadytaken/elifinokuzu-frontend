3005b10822f4d03763792a144484e8eaae93cc4c
Ömer Bülbül <omar.bulbul0@gmail.com>
2018-08-15 20:54:53 +0300

    deneme.html is included in page index.html

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@ function myFunction() {
     document.querySelector("#divLogin").classList.toggle("is-visible");
 }
 
-document.querySelector("#login").addEventListener("click", myFunction);
-
 function myFunction2() {
     document.querySelector("#divSignup").classList.toggle("is-visible");
 }
+function myFunction3() {
+    document.getElementById("id01").style.display = "block";
+}
 
+function myFunction4() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("divLogin").style.display = "none";
+    document.getElementById("submit").style.display = "block";
+    document.getElementById("name").style.display = "block";
+}
+
+document.querySelector("#login").addEventListener("click", myFunction);
 document.querySelector("#signup").addEventListener("click", myFunction2);
+document.querySelector("#submit").addEventListener("click", myFunction3);
+document.querySelector("#btnLogin").addEventListener("click", myFunction4);
